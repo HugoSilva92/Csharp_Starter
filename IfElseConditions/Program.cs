f63c0620d2a61e4989b9121805a1593eaa81787d
HugoSilva92 <hugo.f.o.silva@gmail.com>
2023-09-09 20:37:44 +0100

    exercise done

--- IfElseConditions/Program.cs
+++ IfElseConditions/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+int	daysUntilExpiration = random.Next(12);
+int	discountPercentage = 0;
+
+ if (daysUntilExpiration == 0){
+	Console.WriteLine("Your subscription has expired");
+} else if (daysUntilExpiration == 1){
+	discountPercentage = 20;
+	Console.WriteLine($@"Your subscription expires in a day!
+Renew now and save {discountPercentage}%!");
+} else if (daysUntilExpiration <= 5){
+	discountPercentage = 10;
+	Console.WriteLine($@"Your subscription expires in {daysUntilExpiration} days.
+Renew now and save {discountPercentage}%!");
+} else if (daysUntilExpiration <= 10){
+	Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+Console.WriteLine(daysUntilExpiration);
