1b409ab225841662a8eb3e76244206fb7650a253
HugoSilva92 <hugo.f.o.silva@gmail.com>
2023-09-09 11:51:48 +0100

    First steps in C#

---
+++ MathClass/Program.cs
@@ -0,0 +1,7 @@
+﻿//using System;
+
+int	firstValue = 500;
+int secondValue = 600;
+int largeValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largeValue);
---
+++ typeMethods/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+Random dice = new Random();
+int	roll = dice.Next();
+int	roll1 = dice.Next(4647);
+int	roll2 = dice.Next(-34, 15484684);
+
+Console.WriteLine($"{roll}\n {roll1} \n {roll2}");
+
+dice.Next();
